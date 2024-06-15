bc76dbf8a78d5a58d3825a5d56505b73e17426d4
Jan Wollschlaeger <janmwoll@gmail.com>
2024-06-15 15:27:09 +0200

    first steps towards real MPNN based solubility model

--- sm2/dataset.py
+++ sm2/dataset.py
@@ -1,25 +1,191 @@
 import os, sys
+from smal.all import from_smi
 import numpy as np
+import pandas as pd
 import torch
 from dgl.convert import graph
 from pathlib import Path
+import pandas as pd
+from rdkit import Chem
+import numpy as np
+import os
+from rdkit import Chem, RDConfig, rdBase
+from rdkit.Chem import AllChem, ChemicalFeatures
+import numpy as np
+import os
+import torch
+
+
+import numpy as np
+
+import torch
 
 
 class GraphDataset():
 
-    def __init__(self, npz_file=None, name='nmrshiftdb2'):
-        if npz_file is None:
-            here = Path(__file__).parent
-            npz_file = here / 'data' / 'dataset_graph.npz'
-        self.npz_file = Path(npz_file)
-        assert self.npz_file.exists()
-        self.name = name
-        self.load()
+    def __init__(self, df:pd.DataFrame,):
+        self._read_dataframe(df)
+        self._load()
 
 
-    def load(self):
-        [mol_dict] = np.load(str(self.npz_file), allow_pickle=True)['data']
+    def _read_dataframe(self, df:pd.DataFrame):
+        def add_mol(mol_dict, mol):
 
+            def _DA(mol):
+
+                D_list, A_list = [], []
+                for feat in chem_feature_factory.GetFeaturesForMol(mol):
+                    if feat.GetFamily() == 'Donor': D_list.append(feat.GetAtomIds()[0])
+                    if feat.GetFamily() == 'Acceptor': A_list.append(feat.GetAtomIds()[0])
+                
+                return D_list, A_list
+
+            def _chirality(atom):
+
+                if atom.HasProp('Chirality'):
+                    #assert atom.GetProp('Chirality') in ['Tet_CW', 'Tet_CCW']
+                    c_list = [(atom.GetProp('Chirality') == 'Tet_CW'), (atom.GetProp('Chirality') == 'Tet_CCW')] 
+                else:
+                    c_list = [0, 0]
+
+                return c_list
+
+            def _stereochemistry(bond):
+
+                if bond.HasProp('Stereochemistry'):
+                    #assert bond.GetProp('Stereochemistry') in ['Bond_Cis', 'Bond_Trans']
+                    s_list = [(bond.GetProp('Stereochemistry') == 'Bond_Cis'), (bond.GetProp('Stereochemistry') == 'Bond_Trans')] 
+                else:
+                    s_list = [0, 0]
+
+                return s_list    
+                
+
+            n_node = mol.GetNumAtoms()
+            n_edge = mol.GetNumBonds() * 2
+
+            D_list, A_list = _DA(mol)
+            rings = mol.GetRingInfo().AtomRings()
+            atom_fea1 = np.eye(len(atom_list), dtype = bool)[[atom_list.index(a.GetSymbol()) for a in mol.GetAtoms()]]
+            atom_fea2 = np.eye(len(charge_list), dtype = bool)[[charge_list.index(a.GetFormalCharge()) for a in mol.GetAtoms()]][:,:-1]
+            atom_fea3 = np.eye(len(degree_list), dtype = bool)[[degree_list.index(a.GetDegree()) for a in mol.GetAtoms()]][:,:-1]
+            atom_fea4 = np.eye(len(hybridization_list), dtype = bool)[[hybridization_list.index(str(a.GetHybridization())) for a in mol.GetAtoms()]][:,:-2]
+            atom_fea5 = np.eye(len(hydrogen_list), dtype = bool)[[hydrogen_list.index(a.GetTotalNumHs(includeNeighbors = True)) for a in mol.GetAtoms()]][:,:-1]
+            atom_fea6 = np.eye(len(valence_list), dtype = bool)[[valence_list.index(a.GetTotalValence()) for a in mol.GetAtoms()]][:,:-1]
+            atom_fea7 = np.array([[(j in D_list), (j in A_list)] for j in range(mol.GetNumAtoms())], dtype = bool)
+            atom_fea8 = np.array([_chirality(a) for a in mol.GetAtoms()], dtype = bool)
+            atom_fea9 = np.array([[a.IsInRingSize(s) for s in ringsize_list] for a in mol.GetAtoms()], dtype = bool)
+            atom_fea10 = np.array([[a.GetIsAromatic(), a.IsInRing()] for a in mol.GetAtoms()], dtype = bool)
+            
+            node_attr = np.concatenate([atom_fea1, atom_fea2, atom_fea3, atom_fea4, atom_fea5, atom_fea6, atom_fea7, atom_fea8, atom_fea9, atom_fea10], 1)
+
+            #shift = np.array([atom.GetDoubleProp('shift') for atom in mol.GetAtoms()])
+            #mask = np.array([atom.GetBoolProp('mask') for atom in mol.GetAtoms()])
+
+            mol_dict['n_node'].append(n_node)
+            mol_dict['n_edge'].append(n_edge)
+            mol_dict['node_attr'].append(node_attr)
+
+            #mol_dict['shift'].append(shift)
+            #mol_dict['mask'].append(mask)
+            mol_dict['smi'].append(Chem.MolToSmiles(mol))
+            
+            if n_edge > 0:
+
+                bond_fea1 = np.eye(len(bond_list), dtype = bool)[[bond_list.index(str(b.GetBondType())) for b in mol.GetBonds()]]
+                bond_fea2 = np.array([_stereochemistry(b) for b in mol.GetBonds()], dtype = bool)
+                bond_fea3 = [[b.IsInRing(), b.GetIsConjugated()] for b in mol.GetBonds()]   
+                
+                edge_attr = np.concatenate([bond_fea1, bond_fea2, bond_fea3], 1)
+                edge_attr = np.vstack([edge_attr, edge_attr])
+                
+                bond_loc = np.array([[b.GetBeginAtomIdx(), b.GetEndAtomIdx()] for b in mol.GetBonds()], dtype = int)
+                src = np.hstack([bond_loc[:,0], bond_loc[:,1]])
+                dst = np.hstack([bond_loc[:,1], bond_loc[:,0]])
+                
+                mol_dict['edge_attr'].append(edge_attr)
+                mol_dict['src'].append(src)
+                mol_dict['dst'].append(dst)
+            
+            return mol_dict
+
+
+        df["mol"] = df["solute SMILES"].apply(from_smi)
+        molsuppl = df["mol"].tolist()
+
+        atom_list = ['H', 'Li','B','C','N','O','F','Na','Mg','Al','Si','P','S','Cl','K','Ti','Zn','Ge','As','Se','Br','Pd','Ag','Sn','Sb','Te','I','Hg','Tl','Pb','Bi']
+        charge_list = [1, 2, 3, -1, -2, -3, 0]
+        degree_list = [1, 2, 3, 4, 5, 6, 0]
+        valence_list = [1, 2, 3, 4, 5, 6, 0]
+        hybridization_list = ['SP','SP2','SP3','SP3D','SP3D2','S','UNSPECIFIED']
+        hydrogen_list = [1, 2, 3, 4, 0]
+        ringsize_list = [3, 4, 5, 6, 7, 8]
+
+        bond_list = ['SINGLE', 'DOUBLE', 'TRIPLE', 'AROMATIC']
+
+        rdBase.DisableLog('rdApp.error') 
+        rdBase.DisableLog('rdApp.warning')
+        chem_feature_factory = ChemicalFeatures.BuildFeatureFactory(os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef'))
+
+        mol_dict = {'n_node': [],
+                    'n_edge': [],
+                    'node_attr': [],
+                    'edge_attr': [],
+                    'src': [],
+                    'dst': [],
+                    #'shift': [],
+                    #'mask': [],
+                    'smi': []}
+                        
+        for i, mol in enumerate(molsuppl):
+
+            try:
+                Chem.SanitizeMol(mol)
+                si = Chem.FindPotentialStereo(mol)
+                #import pdb; pdb.set_trace()
+                mol_probe = Chem.AddHs(mol)
+                atom_types = {atm.GetSymbol() for atm in mol_probe.GetAtoms()}
+                if 'H' not in atom_types and 'C' not in atom_types:
+                    print("skipping smiles",Chem.MolToSmiles(mol_probe))
+                    continue
+                for element in si:
+                    if str(element.type) == 'Atom_Tetrahedral' and str(element.specified) == 'Specified':
+                        mol.GetAtomWithIdx(element.centeredOn).SetProp('Chirality', str(element.descriptor))
+                    elif str(element.type) == 'Bond_Double' and str(element.specified) == 'Specified':
+                        mol.GetBondWithIdx(element.centeredOn).SetProp('Stereochemistry', str(element.descriptor))
+                #assert '.' not in Chem.MolToSmiles(mol) # TODO
+            except:
+                raise
+
+            mol = Chem.RemoveHs(mol)
+            mol_dict = add_mol(mol_dict, mol)
+
+            if (i+1) % 1000 == 0: print('%d/%d processed' %(i+1, len(molsuppl)))
+
+        print('%d/%d processed' %(i+1, len(molsuppl)))   
+
+        mol_dict['n_node'] = np.array(mol_dict['n_node']).astype(int)
+        mol_dict['n_edge'] = np.array(mol_dict['n_edge']).astype(int)
+        mol_dict['node_attr'] = np.vstack(mol_dict['node_attr']).astype(bool)
+        mol_dict['edge_attr'] = np.vstack(mol_dict['edge_attr']).astype(bool)
+        mol_dict['src'] = np.hstack(mol_dict['src']).astype(int)
+        mol_dict['dst'] = np.hstack(mol_dict['dst']).astype(int)
+        #mol_dict['shift'] = np.hstack(mol_dict['shift'])
+        #mol_dict['mask'] = np.hstack(mol_dict['mask']).astype(bool)
+        mol_dict['smi'] = np.array(mol_dict['smi'])
+        mol_dict["conc"] = np.array(df["conc"].tolist())
+
+        for key in mol_dict.keys(): 
+            print(key, mol_dict[key].shape, mol_dict[key].dtype)
+        
+        self.mol_dict = mol_dict
+            
+
+
+
+
+    def _load(self):
+        mol_dict = self.mol_dict
         self.n_node = mol_dict['n_node']
         self.n_edge = mol_dict['n_edge']
         self.node_attr = mol_dict['node_attr']
@@ -27,8 +193,9 @@ class GraphDataset():
         self.src = mol_dict['src']
         self.dst = mol_dict['dst']
                 
-        self.shift = mol_dict['shift']
-        self.mask = mol_dict['mask']
+        self.conc = mol_dict["conc"]
+        #self.shift = mol_dict['shift']
+        #self.mask = mol_dict['mask']
         self.smi = mol_dict['smi']
 
         self.n_csum = np.concatenate([[0], np.cumsum(self.n_node)])
@@ -42,10 +209,10 @@ class GraphDataset():
         g.edata['edge_attr'] = torch.from_numpy(self.edge_attr[self.e_csum[idx]:self.e_csum[idx+1]]).float()
 
         n_node = self.n_node[idx:idx+1].astype(int)
-        shift = self.shift[self.n_csum[idx]:self.n_csum[idx+1]].astype(float)
-        mask = self.mask[self.n_csum[idx]:self.n_csum[idx+1]]
+        #shift = self.shift[self.n_csum[idx]:self.n_csum[idx+1]].astype(float)
+        #mask = self.mask[self.n_csum[idx]:self.n_csum[idx+1]]
         
-        return g, n_node, shift, mask
+        return g, n_node, self.conc[idx] #, shift, mask
         
         
     def __len__(self):
--- sm2/model.py
+++ sm2/model.py
@@ -1,3 +1,28 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+from dataset import GraphDataset
+from rdkit import Chem
+from rdkit.Chem import SDWriter
+import numpy as np
+import os
+from rdkit import Chem, RDConfig, rdBase
+from rdkit.Chem import AllChem, ChemicalFeatures
+import numpy as np
+import os
+import torch
+from torch.utils.data import DataLoader
+from dgl.data.utils import split_dataset
+
+from dataset import GraphDataset
+from util import collate_reaction_graphs
+
+from sklearn.metrics import mean_absolute_error,r2_score
+from scipy import stats
+
+
+
+from smal.all import random_fle
 import numpy as np
 import time
 
@@ -13,6 +38,89 @@ from util import MC_dropout
 from sklearn.metrics import mean_absolute_error,r2_score
 from scipy import stats
 
+
+import torch.nn as nn
+
+from dgl.nn.pytorch import Set2Set
+
+from dgllife.model.gnn.mpnn import MPNNGNN
+
+
+class MPNNPredictor(nn.Module):
+    """MPNN for regression and classification on graphs.
+
+    MPNN is introduced in `Neural Message Passing for Quantum Chemistry
+    <https://arxiv.org/abs/1704.01212>`__.
+
+    Parameters
+    ----------
+    node_in_feats : int
+        Size for the input node features.
+    edge_in_feats : int
+        Size for the input edge features.
+    node_out_feats : int
+        Size for the output node representations. Default to 64.
+    edge_hidden_feats : int
+        Size for the hidden edge representations. Default to 128.
+    n_tasks : int
+        Number of tasks, which is also the output size. Default to 1.
+    num_step_message_passing : int
+        Number of message passing steps. Default to 6.
+    num_step_set2set : int
+        Number of set2set steps. Default to 6.
+    num_layer_set2set : int
+        Number of set2set layers. Default to 3.
+    """
+    def __init__(self,
+                 node_in_feats,
+                 edge_in_feats,
+                 node_out_feats=64,
+                 edge_hidden_feats=128,
+                 n_tasks=1,
+                 num_step_message_passing=6,
+                 num_step_set2set=6,
+                 num_layer_set2set=3):
+        super(MPNNPredictor, self).__init__()
+
+        self.gnn = MPNNGNN(node_in_feats=node_in_feats,
+                           node_out_feats=node_out_feats,
+                           edge_in_feats=edge_in_feats,
+                           edge_hidden_feats=edge_hidden_feats,
+                           num_step_message_passing=num_step_message_passing)
+        self.readout = Set2Set(input_dim=node_out_feats,
+                               n_iters=num_step_set2set,
+                               n_layers=num_layer_set2set)
+        self.predict = nn.Sequential(
+            nn.Linear(2 * node_out_feats, node_out_feats),
+            nn.ReLU(),
+            nn.Linear(node_out_feats, n_tasks)
+        )
+
+
+
+    #def forward(self, g, node_feats, edge_feats):
+    def forward(self, g, ):
+        """Graph-level regression/soft classification.
+
+        Parameters
+        ----------
+        g : DGLGraph
+            DGLGraph for a batch of graphs.
+        node_feats : float32 tensor of shape (V, node_in_feats)
+            Input node features.
+        edge_feats : float32 tensor of shape (E, edge_in_feats)
+            Input edge features.
+
+        Returns
+        -------
+        float32 tensor of shape (G, n_tasks)
+            Prediction for the graphs in the batch. G for the number of graphs.
+        """
+        node_feats = g.ndata['node_attr']
+        edge_feats = g.edata['edge_attr']
+        node_feats = self.gnn(g, node_feats, edge_feats)
+        graph_feats = self.readout(g, node_feats)
+        return self.predict(graph_feats)
 
 class nmrMPNN(nn.Module):
 
@@ -61,7 +169,7 @@ class nmrMPNN(nn.Module):
             nn.Linear(hidden_feats, 1)
         )                           
                                
-    def forward(self, g, n_nodes, masks):
+    def forward(self, g, n_nodes):
         
         def embed(g):
             
@@ -86,7 +194,7 @@ class nmrMPNN(nn.Module):
         graph_embed_feats = self.readout(g, node_embed_feats)        
         graph_embed_feats = torch.repeat_interleave(graph_embed_feats, n_nodes, dim = 0)
 
-        out = self.predict(torch.hstack([node_embed_feats, graph_embed_feats])[masks]).flatten()
+        out = self.predict(torch.hstack([node_embed_feats, graph_embed_feats])).flatten()
 
         return out
 
@@ -121,18 +229,17 @@ def training(net, train_loader, val_loader, train_y_mean, train_y_std, model_pat
         start_time = time.time()
         for batchidx, batchdata in enumerate(train_loader):
 
-            inputs, n_nodes, shifts, masks = batchdata
+            inputs, n_nodes, y = batchdata
             
-            shifts = (shifts[masks] - train_y_mean) / train_y_std
+            y = (y - train_y_mean) / train_y_std
             
             inputs = inputs.to(cuda)
             n_nodes = n_nodes.to(cuda)
-            shifts = shifts.to(cuda)
-            masks = masks.to(cuda)
+            y = y.to(cuda)
             
-            predictions = net(inputs, n_nodes, masks)
+            predictions = net(inputs,) # n_nodes, y)
             
-            loss = torch.abs(predictions - shifts).mean()
+            loss = torch.abs(predictions - y).mean()
             
             optimizer.zero_grad()
             loss.backward()
@@ -177,10 +284,69 @@ def inference(net, test_loader, train_y_mean, train_y_std, n_forward_pass = 30, 
         
             inputs = batchdata[0].to(cuda)
             n_nodes = batchdata[1].to(cuda)
-            masks = batchdata[3].to(cuda)
 
-            tsty_pred.append(np.array([net(inputs, n_nodes, masks).cpu().numpy() for _ in range(n_forward_pass)]).transpose())
+            tsty_pred.append(np.array([net(inputs, n_nodes).cpu().numpy() for _ in range(n_forward_pass)]).transpose())
 
     tsty_pred = np.vstack(tsty_pred) * train_y_std + train_y_mean
     
     return np.mean(tsty_pred, 1)
+
+
+
+
+def run_for_smiles(smis:list[str],):
+    data_split = [0.8, 0.1, 0.1]
+    batch_size = 128
+    use_pretrain = True
+
+    here = Path(__file__).parent
+    model_path = str(here / 'checkpoints' / 'model.pt')
+    random_seed = 1
+    #if not os.path.exists(model_path): os.makedirs(model_path)
+    data_pred = pd.DataFrame({"solute SMILES": smis, "conc": [0 for _ in smis]})
+    data_pred = GraphDataset(data_pred)
+    data = pd.read_csv(here /  "data" / "training_data_singleton.csv")
+    data = GraphDataset(data)
+    train_set, val_set, test_set = split_dataset(data, data_split, shuffle=True, random_state=random_seed)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, collate_fn=collate_reaction_graphs, drop_last=True)
+    val_loader = DataLoader(dataset=val_set, batch_size=batch_size, shuffle=False, collate_fn=collate_reaction_graphs)
+    test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False, collate_fn=collate_reaction_graphs)
+    pred_loader = DataLoader(dataset=data_pred, batch_size=batch_size, shuffle=False, collate_fn=collate_reaction_graphs)
+
+    train_y = np.hstack([inst[-2][inst[-1]] for inst in iter(train_loader.dataset)])
+    train_y_mean = np.mean(train_y)
+    train_y_std = np.std(train_y)
+
+    node_dim = data.node_attr.shape[1]
+    edge_dim = data.edge_attr.shape[1]
+    net = MPNNPredictor(node_dim, edge_dim).cuda()
+
+    print('-- CONFIGURATIONS')
+    print('--- data_size:', data.__len__())
+    print('--- train/val/test: %d/%d/%d' %(train_set.__len__(), val_set.__len__(), test_set.__len__()))
+    print('--- use_pretrain:', use_pretrain)
+    print('--- model_path:', model_path)
+
+
+    # training
+    if not use_pretrain:
+        print('-- TRAINING')
+        net = training(net, train_loader, val_loader, train_y_mean, train_y_std, model_path)
+    else:
+        print('-- LOAD SAVED MODEL')
+        net.load_state_dict(torch.load(model_path))
+
+
+    # inference
+    test_y = np.hstack([inst[-2][inst[-1]] for inst in iter(test_loader.dataset)])
+    test_y_pred = inference(net, test_loader, train_y_mean, train_y_std)
+    test_mae = mean_absolute_error(test_y, test_y_pred)
+
+    print('-- RESULT')
+    print('--- test MAE', test_mae)
+    return inference(net,pred_loader, train_y_mean, train_y_std)
+
+
+if __name__ == "__main__":
+    run_for_smiles(["CCOCC"])
